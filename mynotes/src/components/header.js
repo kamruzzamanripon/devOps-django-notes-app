e7cfb68a1e8bd15008cd635762ae14a5080a23c1
syed kamruzzaman <30769906+kamruzzamanripon@users.noreply.github.com>
2024-11-28 02:18:12 +0600

    Update header.js

--- mynotes/src/components/header.js
+++ mynotes/src/components/header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div className="app-header">
-            <h1>My Notes Power by RI</h1>
+            <h1>My Notes Power by RI ++</h1>
         </div>
     )
 }
